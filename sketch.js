dbf005e8a761bb43ce1e8dccdce11f2e8e25153f
zturtledog <71353802+zturtledog@users.noreply.github.com>
2022-03-01 18:12:06 -0800

    setup a p5 sketch that is good

--- sketch.js
+++ sketch.js
@@ -1,1 +1,14 @@
 //p5 sketch
+
+function setup() {
+    createCanvas(windowWidth,windowHeight)
+}
+
+function draw() {
+    //draw
+    background("#bbbbbb")
+}
+
+function windowResized() {
+    resizeCanvas(windowWidth, windowHeight);
+}
